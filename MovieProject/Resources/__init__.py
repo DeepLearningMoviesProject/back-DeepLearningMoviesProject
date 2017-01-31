10c98fa8145270aacae4dfde445482f4e4c43a46
Julian H <hattinguais.julian@gmail.com>
2017-01-31 13:50:14 +0100

    Refactor (add Top lovel package)

---
+++ MovieProject/Resources/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 30 11:09:56 2017
+
+@author: Julian
+"""
+
+from os.path import dirname, abspath, join
+
+RES_PATH = dirname(abspath(__file__))
+
+GENRES_FILE = join(RES_PATH, "genres.npy")
