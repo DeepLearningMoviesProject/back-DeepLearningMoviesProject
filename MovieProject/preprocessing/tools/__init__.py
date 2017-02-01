3cf5eaf5a46198def0d99556a90839712a51a750
Edwin Niogret <darkedwin38@hotmail.fr>
2017-02-01 22:39:57 +0800

    Correction minor bug
    Correctio

--- MovieProject/preprocessing/tools/__init__.py
+++ MovieProject/preprocessing/tools/__init__.py
@@ -9,5 +9,6 @@ Created on Mon Jan 30 11:09:56 2017
 from .gloveDict import * 
 from .createCorpusOfAbtracts import *
 from .trainD2VOnCorpus import *
- 
+from .apiTMDB import *
+
 __all__ = [s for s in dir() if not s.startswith('_')]
