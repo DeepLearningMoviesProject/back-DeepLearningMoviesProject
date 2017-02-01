d9491960879aa2db94b96679b376d19616c35e29
Féanor <Fanor>
2017-02-01 21:38:13 +0800

    Preprocessing methodes for Keywords/Overview/Titles/Rating matric for Keras. Also adding an example /tests/examplePreprocessing.py

---
+++ MovieProject/tests/examplePreprocessing.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 23:32:06 2017
+
+@author: darke
+"""
+
+from MovieProject.preprocessing.preprocessing import *
+
+
+
+def main():
+    """
+       This is an example of how to use preprocess function
+    """
+
+    i = 1
+    idArray = []
+    while (i<15):
+        idArray.append(i)
+        i += 1
+    result = preprocess(idArray)
+    
+    print result
+    
+    
+if __name__ == "__main__" :
+    main()
+    
