18b15e83c61c4a6762d93cc8f85130ca61df9d9b
Edwin Niogret <darkedwin38@hotmail.fr>
2017-02-03 17:55:18 +0800

    Example of how to save a 2D array. Useful to not load movies datas

--- MovieProject/tests/examplePreprocessing.py
+++ MovieProject/tests/examplePreprocessing.py
@@ -7,7 +7,8 @@ Created on Tue Jan 31 23:32:06 2017
 
 from MovieProject.preprocessing.preprocessing import *
 
-
+import numpy as np
+import pickle 
 
 def main():
     """
@@ -20,8 +21,13 @@ def main():
         idArray.append(i)
         i += 1
     result = preprocess(idArray)
-    
-    print result
+    with open('test.data', 'w') as f:
+        pickle.dump(result, f)
+
+
+    with open('test.data', 'r') as f:
+        new_data = pickle.load(f)
+    print new_data
 
     
         
