bf4e959ebaf9d747f3e37f60d4a3fc62803502a1
Julian H <hattinguais.julian@gmail.com>
2017-02-13 11:52:48 +0100

    Documentation and cleaning of createCorpus

--- MovieProject/preprocessing/tools/createCorpusOfAbtracts.py
+++ MovieProject/preprocessing/tools/createCorpusOfAbtracts.py
@@ -17,20 +17,22 @@ import tmdbsimple as tmdb
 # To pre-process texte in each abtract
 from MovieProject.preprocessing import texts
 
-from os.path import isdir
+from os.path import isdir, join
 from nltk.data import path
 
+
 tmdb.API_KEY = 'ff3f07bf3577a496a2f813488eb29980'
-filePath = "../../resources/train_overviews_treated.txt"
-#filePath = "../../resources/train_overviews_noStopWords.txt"
 
-
-"""
-Load and store abstracts on TMDB, in texte file
-"""
-def loadResumes(fileName, pages_max):
+def loadResumes(filename, pagesMax):
+    """
+        Load and store abstracts on TMDB, in text file
+        
+        Parameters:
+            filename -> String, name of the file where resumes will be stored
+            pagesMax -> int, number of page to download from tmdb
+    """
     
-    file_abstracts = codecs.open(fileName, "w", 'utf-8')
+    fileAbstracts = codecs.open(filename, "w", 'utf-8')
     
     # Download stopwords if not present
     if not isdir(path[0]):
@@ -39,25 +41,23 @@ def loadResumes(fileName, pages_max):
     
     cachedStopWords = stopwords.words("english")
     
-    for i in range(1,pages_max):
+    for i in range(1,pagesMax):
         response = discover.movie(page=i)
-        films_nb = len(response[u'results'])
-        for j in range (0,films_nb):
+        filmsNb = len(response[u'results'])
+        for j in range (filmsNb):
             abstract = response[u'results'][j][u'overview']
             abstract = texts.preProcessingAbstracts(abstract)
-            file_abstracts.write(abstract+'\n')
+            fileAbstracts.write(abstract+'\n')
         print i
     
-    file_abstracts.close()
+    fileAbstracts.close()
     
     
 if __name__ == "__main__":
+    from MovieProject.resources import RES_PATH
     
-    discover = tmdb.Discover()
-    response = discover.movie(page=1)
-    #pages_nb = int(response[u'total_pages'])
-    #print pages_nb   
-    
+    filePath = join(RES_PATH, "train_overviews_treated.txt")
+
     loadResumes(filePath, 1000)
     
 
