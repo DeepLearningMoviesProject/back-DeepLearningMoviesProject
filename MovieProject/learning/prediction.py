f7ae5db74130e91368c76e643869799f975f0b6a
Elsa Navarro <elsa0navarro@gmail.com>
2017-03-03 11:49:21 +0100

    Last fix on prediction, unit test for prediction is OK

--- MovieProject/learning/prediction.py
+++ MovieProject/learning/prediction.py
@@ -16,16 +16,18 @@ import tmdbsimple as tmdb
 
 batch = 500
 
-def predict(movies, model, **kwargs):
+def predictMovies(movies, model, **kwargs):
     '''
     Predicts the class of the movie according to the model
         parameters : 
             - movies : an array of the id of the movie we want to know the class of, the id must exist
-            - model : the model that matches the taste of the user
+            - model : the model that matches the taste of the user, it must not be None
         returns : 
-            - a boolean to tell if the movie is liked or not
+            - a np.array of values between 0 and 1, that show how much a movie is likely to be loved
     '''
-    
+    if model is None:
+        raise ValueError('The model ', model, ' is not defined!')
+
     pProcessor = Preprocessor(**kwargs)
     
     data = pProcessor.preprocess(movies)
@@ -67,7 +69,6 @@ def suggestNMovies(model, n, **kwargs):
             page = tmdb.Discover().movie(page=iPage, vote_count_gte=20)
             result = page["results"]
             iMovie = randint(0,len(result)-1)
-            print iMovie, " in ", len(result), " movies"
             resMovie = result[iMovie]
             idMovie = resMovie['id']
             #We check that we didn't already checked the movie
@@ -80,7 +81,7 @@ def suggestNMovies(model, n, **kwargs):
                 
         #Now we have a movies list of the movies we need to find
         #We compute the predictions matching the model to know which movies to keep
-        predictions = predict(moviesIds, model, **kwargs)
+        predictions = predictMovies(moviesIds, model, **kwargs)
 
         print "predictions done, we sort the results to keep"
         
@@ -101,24 +102,4 @@ def suggestNMovies(model, n, **kwargs):
             i += 1
         toFind -= added
 
-    return suggestion
-    
-if __name__ == '__main__':
-    movie ={}
-    
-    page = tmdb.Discover().movie(vote_count_gte=20)
-    
-    movie = page["results"][3]
-    movie2 = page["results"][9]
-#    movie = {u'poster_path': u'/nEg3B0YySjSgoJCntuPzYKNOzGm.jpg', u'title': u'The Black Cat', u'overview': u'American honeymooners in Hungary are trapped in the home of a Satan-worshiping priest when the bride is taken there for medical help following a road accident.', u'release_date': u'1934-05-07', u'popularity': 1.464678, u'original_title': u'The Black Cat', u'backdrop_path': u'/bcs1IYkT63LRYvR1yysnb0GKwzT.jpg', u'vote_count': 37, u'video': False, u'adult': False, u'vote_average': 6.7, u'original_language': u'en', u'id': 24106, u'genre_ids': [27, 80]}
-    
-    movie[u'accuracy'] = 0.22
-    movie2[u'accuracy'] = 0.56
-    
-    sugg = []
-    
-    sugg.append(movie)
-    sugg.append(movie2)
-    
-    print sugg
-    
+    return suggestion    
--- MovieProject/tests/unit/test_prediction.py
+++ MovieProject/tests/unit/test_prediction.py
@@ -9,61 +9,98 @@ Created on Fri Feb 17 13:26:55 2017
 
 import unittest
 from MovieProject.preprocessing import Preprocessor
-from MovieProject.learning import buildModel, predict, suggestNMovies
+from MovieProject.learning import buildModel, predictMovies, suggestNMovies
 import numpy as np
+import tmdbsimple as tmdb
+from random import randint
 
-#movies = {"11":1,"18":1,"22":1}
-#ids = [int(key) for key in movies]
-#labels = np.array([movies[key] for key in movies])
-#params = { "titles":False,
-#           "rating":True,
-#           "overviews":True,
-#           "keywords":True,
-#           "genres":True,
-#           "actors":False,
-#           "directors":True,
-#          "compagnies" : False,
-#          "language" : True,
-#          "belongs" : True,
-#          "runtime" : False,
-#          "date" : True }
+params = { "titles":True,
+          "rating":True,
+          "overviews":True,
+          "keywords":True,
+          "genres":True,
+          "actors":True,
+          "directors":True,
+         "compagnies" : True,
+         "language" : True,
+         "belongs" : True,
+         "runtime" : True,
+         "date" : True }
 
 
 class PredictionTest(unittest.TestCase):
     """Test case used to test the module 'learning.prediction'."""
 
-    def test_predict(self):
-        """Tests 'prediction.predict'."""
-        movies = {"11":1,"18":1,"22":1}
-        ids = [int(key) for key in movies]
-        labels = np.array([movies[key] for key in movies])    
-        params = { "titles":True,
-                   "rating":True,
-                   "overviews":True,
-                   "keywords":True,
-                   "genres":True,
-                   "actors":True,
-                   "directors":True,
-                  "compagnies" : True,
-                  "language" : True,
-                  "belongs" : True,
-                  "runtime" : True,
-                  "date" : True }
-        pProcessor = Preprocessor(**params)
-        data = pProcessor.preprocess(ids)
+    model = None
+    idMovie1 = 0
+    idMovie2 = 0
+
+    # This method will be called BEFORE every test
+    def setUp(self):
+
+        #Pick 2 random movies
+        pages = tmdb.Discover().movie(vote_count_gte=20)
+        totalPages = pages['total_pages'] 
+        iPage1 =  randint(1,totalPages)
+        iPage2 =  randint(1,totalPages)
+        page1 = tmdb.Discover().movie(page=iPage1, vote_count_gte=20)
+        page2 = tmdb.Discover().movie(page=iPage2, vote_count_gte=20)
+        result1 = page1["results"]
+        result2 = page2["results"]
+        iMovie1 = randint(0,len(result1)-1)
+        iMovie2 = randint(0,len(result2)-1)
+        self.idMovie1 = result1[iMovie1]['id']
+        self.idMovie2 = result2[iMovie2]['id']
+
+        #We build the model only once
+        if self.model is None:
+            #Movies to build the model
+            movies = {"11":1,"18":1,"22":1}
+            ids = [int(key) for key in movies]
+            labels = np.array([movies[key] for key in movies])
+
+            #We will build the model
+            pProcessor = Preprocessor(**params)
+            data = pProcessor.preprocess(ids)
+            self.model = buildModel(data, labels)
+ 
+    # this method will be called AFTER every test
+    def tearDown(self):
+        self.movie1 = {}
+        self.movie1 = {}
+
+    def test_predictMovies(self):
+        """Tests 'prediction.predictMovies'."""
         
-        model = buildModel(data, labels)
-        
-        print "start predict"
+        #Regular movies - they exist in tmdb
+        toPredict = [self.idMovie1, self.idMovie2]
+        predictionsValid = predictMovies(toPredict, self.model, **params)
+        pValidValue1 = predictionsValid[0].item()
+        pValidValue2 = predictionsValid[1].item()
+        print predictionsValid
+        print pValidValue1
+        print pValidValue2
+        #No movie
         toPredict = []
-        #Non existant id
-        #No id
-        #Movie with specials characters, empty strings
-#        predictions = suggestNMovies(toPredict, model, **params)
+        predictionsEmpty = predictMovies(toPredict, self.model, **params)
+        #Movie with incorrect ID
+        toPredict = [1]
+        predictionsInvalid = predictMovies(toPredict, self.model, **params)
+        #Try to predict movies with an invalid model (None)
+        with self.assertRaises(ValueError):
+            predictionsValidModelInvalid = predictMovies(toPredict, None, **params)
         
-#        self.assertEqual(predictions, toPredict)
-#        self.assertEqual(toPredict, [])
+        self.assertEqual(predictionsEmpty, [])
+        self.assertEqual(predictionsInvalid, [])
+        self.assertTrue(pValidValue1 <= 1.0 and pValidValue1 >= 0)
+        self.assertTrue(pValidValue2 <= 1.0 and pValidValue2 >= 0)
 
     def test_suggestNMovies(self):
-    	"""Tests 'prediction.pickNMovies'."""
+    	"""Tests 'prediction.suggestNMovies'."""
+        suggestedEmpty = suggestNMovies(self.model, 0, **params)
+        suggested20 = suggestNMovies(self.model, 20, **params)
+
+        self.assertEqual(suggestedEmpty, [])
+        self.assertEqual(len(suggested20), 20)
+        self.assertEqual(type(suggested20), type([]))
      
