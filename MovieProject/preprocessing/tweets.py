bd176c98e2d0b4519516d7729769e05e395ab1f3
Coralie RACHEX <rachex.coralie@gmail.com>
2017-03-08 16:22:45 +0100

    unit tests on tweets.py

--- MovieProject/preprocessing/tweets.py
+++ MovieProject/preprocessing/tweets.py
@@ -18,7 +18,7 @@ import re
 list_punctuation = [",",".","!","?","[","]","(",")","{","}","-",'"',"'",":","$","\\","/",";","+","=","&","<",">","@","*","_"] 
                      
  
-def preprocessTweet(tweet, dico): 
+def preprocessTweet(tweet, dico, encod='utf-8'): 
     """ 
     Preprocess the tweets
         Parameters :  
@@ -27,16 +27,20 @@ def preprocessTweet(tweet, dico):
         Return : 
             - the tweet preprocessed 
     """ 
+    
+    if not isinstance(tweet, unicode):
+        tweet = unicode(tweet, encod,'replace')
+        
     # Convert to lower case 
     tweet = tweet.lower()
     # Remove accentued caracters
     tweet = withoutAccents(tweet) 
     # Remove all useless words (hashtag, user, url)
-    tweet = convertUselessWords(tweet)
+    tweet = _convertUselessWords(tweet)
     # Remove all punctuation of list_punctuation 
     tweet = "".join(c for c in tweet if c not in list_punctuation)
     # Remove caractere repetition (more than 3 repetition only)
-    tweet = removeRepetitions(tweet)
+    tweet = _removeRepetitions(tweet)
     # Check if words are present in the dictionnary
     listWords=[]
     words = tweet.split()
@@ -47,7 +51,7 @@ def preprocessTweet(tweet, dico):
     return ' '.join(listWords)
  
      
-def removeRepetitions(s): 
+def _removeRepetitions(s, encod='utf-8'): 
     """ 
     Look for 3 or more repetitions of character and replace with the character itself.
     Remove also additional white spaces and replace by only one white space.
@@ -56,14 +60,18 @@ def removeRepetitions(s):
         Return :  
             - the string without any 3 or more caracters repetitions and without additional white spaces
     """ 
+    if not isinstance(s, unicode):
+        s = unicode(s, encod,'replace')
+        
     # Remove additional caracters 
     s = re.sub(r'(\w)\1{2,100}', r'\1', s) 
     # Remove additional white spaces 
     s = re.sub( '\s+', ' ', s ).strip() 
+    
     return s 
      
      
-def convertUselessWords(s): 
+def _convertUselessWords(s, encod='utf-8'): 
     """ 
     Remove useless expressions on tweets like urls and @username.
     Convert #word to word
@@ -72,27 +80,36 @@ def convertUselessWords(s):
         Return :  
             - the string without any useless words 
     """ 
+    if not isinstance(s, unicode):
+        s = unicode(s, encod,'replace')
+    
     #Remove www.* or https?://* 
     s = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','',s) 
     #Remove @username
     s = re.sub('@[^\s]+','',s) 
     #Replace #word with word 
     s = re.sub(r'#([^\s]+)', r'\1', s) 
+    
     return s 
 
     
-def removeMovie(tweet, title):
+def removeMovie(tweet, title, encod='utf-8'): 
     """
     Remove movie title in the tweet, so as not to distort the results
         Parameters :
             - tweet : string of the tweet
             - title : title of the movie
     """
+    if not isinstance(tweet, unicode):
+        tweet = unicode(tweet, encod,'replace')
+    if not isinstance(title, unicode):
+        title = unicode(title, encod,'replace')
     # Convert to lower case 
-    tweet = tweet.lower()
-    title = title.lower()
-    # Remove accentued caracters
+    tweet = withoutAccents(tweet.lower())
+    title = withoutAccents(title.lower())
+    # Remove movie title
     tweet = re.sub(title,'',tweet) 
+    
     return tweet
     
     
