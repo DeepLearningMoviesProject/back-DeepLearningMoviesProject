31ad9874a1401df2f8bdb4193f989867d89e902f
Coralie RACHEX <rachex.coralie@gmail.com>
2017-03-08 15:03:51 +0100

    Add unit tests for texts.py

--- MovieProject/tests/unit/test_texts.py
+++ MovieProject/tests/unit/test_texts.py
@@ -14,13 +14,41 @@ class TextTest(unittest.TestCase):
 
     """Test case used to test the module 'preprocessing.text'."""
 
-    def test_withoutAccents(self):
+    def test_withAccents(self):
         """Tests 'text.withoutAccents'."""
         wordInit = "àAâäO0jkfåçsgqéhgèêhëjîjïkôöùkûèoüÿxcğ"
         wordFinal = "aAaaO0jkfacsgqehgeehejijikooukueouyxcg"
         word = texts.withoutAccents(wordInit)
         self.assertEquals(word, wordFinal)
+        
+    def test_withoutAccents(self):
+        """Tests 'text.withoutAccents'."""
+        wordInit = "aAaaO0jkfacsgqehgeehejijikooukueouyxcg"
+        wordFinal = "aAaaO0jkfacsgqehgeehejijikooukueouyxcg"
+        word = texts.withoutAccents(wordInit)
+        self.assertEquals(word, wordFinal)
+        
+    def test_acsii(self):
+        """Tests 'text.withoutAccents'."""
+        wordInit = u'aefzgrag'.encode("ascii")
+        wordFinal = "aefzgrag"
+        word = texts.withoutAccents(wordInit)
+        self.assertEquals(word, wordFinal)
 
+    def test_unicode(self):
+        """Tests 'text.withoutAccents'."""
+        wordInit = u'aefzgrag'
+        wordFinal = "aefzgrag"
+        word = texts.withoutAccents(wordInit)
+        self.assertEquals(word, wordFinal)
+        
+    def test_empty(self):
+        """Tests 'text.withoutAccents'."""
+        wordInit = ""
+        wordFinal = ""
+        word = texts.withoutAccents(wordInit)
+        self.assertEquals(word, wordFinal)
+        
     def test_preProcessingAbstracts(self):
     	"""Tests 'text.preProcessingAbstracts'."""
         abstractInit = "Hello here! J'ai demandé un café noir... C'est pour demain ?"
