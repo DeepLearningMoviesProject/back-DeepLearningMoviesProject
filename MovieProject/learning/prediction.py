bcff7e30580cd50370db37f02c363779b6e4a429
Elsa Navarro <elsa0navarro@gmail.com>
2017-03-10 15:00:28 +0100

    Get pages from TMDB in French

--- MovieProject/learning/prediction.py
+++ MovieProject/learning/prediction.py
@@ -68,7 +68,7 @@ def suggestNMovies(model, n, **kwargs):
         while(len(movies) < toFind):
             #We pick a random page and a random movie in this page
             iPage =  randint(1,totalPages)
-            page = tmdb.Discover().movie(page=iPage, vote_count_gte=20)
+            page = tmdb.Discover().movie(page=iPage, vote_count_gte=20, language='fr-FR')
             result = page["results"]
             iMovie = randint(0,len(result)-1)
             resMovie = result[iMovie]
@@ -108,105 +108,3 @@ def suggestNMovies(model, n, **kwargs):
         nTry += 1
 
     return suggestion    
-
-
-#def saveModel(username, model):
-#    """
-#        Save model on resource/persist/model/username_model.json
-#
-#        username : unique key for a user, and a model is unique for a user
-#        model : the keras model for the user
-#    """
-#    # serialize model to JSON
-#    model_json = model.to_json()
-#    model_filepath = RES_MODEL_PATH + '/' + username + '_model'
-#    
-#    #If the model directory doesn't exists, we create it
-#    if not exists(RES_MODEL_PATH):
-#        makedirs(RES_MODEL_PATH)
-#
-#    with open(model_filepath + '.json', "w") as json_file:
-#        json_file.write(model_json)
-#
-#    # serialize weights to HDF5
-#    model.save_weights(model_filepath + '.h5')
-#
-#    print("Saved model to disk")
-#
-#def loadModel(username):
-#    """
-#        Save model on resource/persist/model/username_model.json
-#
-#        username : unique key for a user, and a model is unique for a user
-#        model : the keras model for the user
-#    """
-#    # serialize model to JSON
-#   # model_json = model.to_json()
-#    model_filepath = RES_MODEL_PATH + '/' + username + '_model'
-#    
-#    #If file doesn't exists, we return None
-#    if(isfile(model_filepath + '.json') and isfile(model_filepath + '.h5')):
-#        # load json and create model
-#        json_file = open(model_filepath + '.json', 'r')
-#        loaded_model_json = json_file.read()
-#        json_file.close()
-#        loaded_model = model_from_json(loaded_model_json)
-#        # load weights into new model
-#        loaded_model.load_weights(model_filepath + '.h5')
-#        print("Loaded model from disk")
-#        
-#        return loaded_model
-#    # evaluate loaded model on test data
-#    # loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-#    # score = loaded_model.evaluate(X, Y, verbose=0)
-#    else :
-#        print "The model doesn't exists"
-#        return None
-#
-#
-#def getIdFromLikedMovies(username, isLiked):
-#    """
-#    
-#    """
-#    
-#    movies = dbManager.getMoviesLikedByUser(username,isLiked)
-#    return { str(movie.idMovie) : int(movie.liked) for movie in movies}
-
-if __name__ == '__main__':
-    
-    params = { "titles":True,
-               "rating":True,
-               "overviews":True,
-               "keywords":True,
-               "genres":True,
-               "actors":True,
-               "directors":True,
-              "compagnies" : True,
-              "language" : True,
-              "belongs" : True,
-              "runtime" : True,
-              "date" : True }
-
-        #Retrieve the user movies
-#    username = g.user_name
-    username = 'User1'
-    userMovies = getIdFromLikedMovies(username, None)
-
-    #extract the ids and the labels of each movie
-    ids = [int(key) for key in userMovies]
-    labels = np.array([userMovies[key] for key in userMovies])
-    
-    print "Movies extracted"
-    
-    pProcessor = Preprocessor(**params)
-
-    #preprocess data
-    data = pProcessor.preprocess(ids)
-    
-    print "Movies loaded, building model"
-    
-    model = buildModel(data, labels)
-
-    print "Saving model to file ..."
-    
-    saveModel(username, model)
